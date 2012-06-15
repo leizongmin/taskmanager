b23135ba6f919972e197923abde5e7ee5c3cb944
Lei Zongmin <leizongmin@gmail.com>
2012-06-15 17:24:53 +0800

    增加在线日志功能

--- public/index.js
+++ public/index.js
@@ -1,13 +1,48 @@
 $(document).ready(function () {
   
-  var windowResize = function () {
-    $w = $(window);
-    $h = $('.top-bar');
-    $me = $('#main-iframe');
-    $me.height($w.height() - $h.height() - 28);
+  // 界面
+  (function () {
+    var windowResize = function () {
+      $w = $(window);
+      $h = $('.top-bar');
+      $me = $('#main-iframe');
+      $me.height($w.height() - $h.height() - 28);
+      $('#online-logs .log-lines').height($('#online-logs').height() - 50);
+    };
+    $(window).resize(windowResize);
+    windowResize();
+  })();
+    
+});
+
+
+// 在线日志
+(function () {
+  var socket;
+  
+  var connectToServer = function () {
+    $.getJSON('/admin/api/socket.io/key', {r: Math.random()}, function (d) {
+       socket = io.connect('http://localhost/?key=' + d.key);
+       socket.on('connect', onConnection)
+             .on('disconnect', onDisconnect)
+             .on('line', onLine);
+    });
   };
   
-  $(window).resize(windowResize);
-  windowResize();
+  var onConnection = function () {
+    console.log('连接服务器成功。');
+  };
   
-});
+  var onDisconnect = function () {
+    console.log('与服务器断开连接！');
+  };
+  
+  var $out = $('#online-logs .log-lines');
+  var onLine = function (msg) {
+    $out.append('<pre>' + msg + '</pre>')
+    $out.scrollTop(100000);
+    console.log(msg);
+  };
+    
+  connectToServer();
+})();

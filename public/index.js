4e18cbe7ffba559ca4e6706d2746580d8e958bb3
Lei Zongmin <leizongmin@gmail.com>
2012-06-16 11:34:01 +0800

    控制台输出功能可标出是由哪个应用输出的

--- public/index.js
+++ public/index.js
@@ -21,11 +21,13 @@ $(document).ready(function () {
   var socket;
   
   var connectToServer = function () {
+    console.log('获取连接密码...');
     $.getJSON('/admin/api/socket.io/key', {r: Math.random()}, function (d) {
-       socket = io.connect('http://localhost/?key=' + d.key);
-       socket.on('connect', onConnection)
-             .on('disconnect', onDisconnect)
-             .on('line', onLine);
+      console.log('连接到服务器...');
+      socket = io.connect('http://localhost/?key=' + d.key);
+      socket.on('connect', onConnection)
+            .on('disconnect', onDisconnect)
+            .on('line', onLine);
     });
   };
   
@@ -35,26 +37,46 @@ $(document).ready(function () {
   
   var onDisconnect = function () {
     console.log('与服务器断开连接！');
+    connectToServer();
   };
   
   var $out = $('#online-logs .log-lines');
-  var color = {
-    info:   'cyan',
-    debug:  'green',
-    error:  'red',
-    warn:   'yellow'
-  };
   var onLine = function (msg, type) {
     if (typeof msg !== 'string')
-      return console.error('接收信息出错：', msg, type);
-    var c = color[type];
-    if (c)
-      $out.append('<pre style="color:' + c + '">' + msg + '</pre>');
+      return console.error('接收信息出错：', msg, type, appname);
+    if (!type)
+      type = 'log';
+    else {
+      var _type = type.split(':');
+      if (_type.length >= 2) {
+        var appname = _type[0];
+        type = _type[1];
+      }
+    }
+    if (logFilterSwitch[type] === false)
+      var style = 'display: none;';
     else
-      $out.append('<pre>' + msg + '</pre>');
+      var style = '';
+    if (appname)
+      msg = '<span class="log-appname">' + appname + '</span>' + msg;
+    $out.append('<pre class="logtype-' + type + '" style="' + style + '">' + msg + '</pre>');
     $out.scrollTop(1000000);
-    console.log(msg);
+    // console.log(msg);
   };
     
   connectToServer();
 })();
+
+// 过滤日志
+var logFilterSwitch = {};
+var logFilter = function (type, obj) {
+  var $me = $(obj);
+  if ($me.attr('checked')) {
+    logFilterSwitch[type] = true;
+    $('.logtype-' + type).show();
+  }
+  else {
+    logFilterSwitch[type] = false;
+    $('.logtype-' + type).hide();
+  }
+};
